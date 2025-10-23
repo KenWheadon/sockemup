0a81cb71e1df9fdb3b224e3009e53845cdec5474
KenWheadon <ken@smartfirm.com>
2025-10-23 00:54:15 -0300

    fixed base game anim for martha

--- config.js
+++ config.js
@@ -87,6 +87,7 @@ const GameConfig = {
     rows: 6,
     totalFrames: 36,
     animationFrames: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], // Which frames to use in the animation sequence
+    fps: 12, // 12 frames per second for running animation
   },
 
   MARTHA_LAUGHING_SPRITESHEET: {
